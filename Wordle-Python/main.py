448b9b9919cd16398dbe824b82ce38b4c75efd8b
Derek <dduong03@uoguelph.ca>
2024-01-23 14:35:36 -0500

    refactored code into methods

--- Wordle-Python/main.py
+++ Wordle-Python/main.py
@@ -1,22 +1,60 @@
+import pathlib
+import random
+import sys
+from string import ascii_letters
+g_FILENAME = "wordlist.txt"
+ 
+def main():
+    # Pre-process
+    word = get_random_word()
 
-word = "TESTVALUE"
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
 
-for user_guess in range(1,7):
-    guess = input("Guess a letter: ").upper()
-    
-    #If the guess is correct, end the game
-    if guess == word:
-        print("Correct!")
-        break
-    
+        show_guess()
+        if guess == word:
+            break
+
+    # Post-process
+    else:
+        game_over()
+
+
+def show_guess(guess, word):
     correct_letters = {
         letter for letter, correct in zip(word, guess) if correct == letter
     }
-    
-
     misplaced_letters = set(guess) & set(word) - correct_letters
     wrong_letters = set(guess) - set(word)
+        
+    print("correct Letters: ", "".join(sorted(correct_letters)))    
+    print("misplaced Letters: ", "".join(sorted(misplaced_letters)))
+    print("wrong Letters: ", "".join(sorted(wrong_letters)))
     
-    print(f"Correct letters: {correct_letters}")
-    print(f"Misplaced letters: {misplaced_letters}")
-    print(f"Wrong letters: {wrong_letters}")
+
+def game_over(word):
+    print("The word was: ", word)
+    print("Game Over!")
+
+#returns the word to be guessed by the user
+#Grabs the list of random words from the wordlist.txt file
+def get_random_word():
+    word_list = pathlib.Path(__file__).parent / g_FILENAME
+    sorted_words = sorted( # Sort words alphabetically
+        # Read all words from the file
+        {
+            word.lower()
+            # Filter out words that contain non-ASCII letters
+            for word in word_list.read_text(encoding="utf-8").split()
+            if all (letter in ascii_letters for letter in word) and (len(word) == 5)
+        },
+        # Sort words by length
+        key=lambda word: (len(word), word), 
+    )
+    # Write the sorted words to the output file
+    # out_path.write_text("\n".join(words), encoding="utf-8")
+    return random.choice(sorted_words)
+
+if __name__ == "__main__":
+    main()
