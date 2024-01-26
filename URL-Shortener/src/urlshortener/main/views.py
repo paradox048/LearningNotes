bbca16585932a325fde2177a11781c169f5d472a
Derek <dduong03@uoguelph.ca>
2024-01-26 09:00:35 -0500

    update

--- URL-Shortener/src/urlshortener/main/views.py
+++ URL-Shortener/src/urlshortener/main/views.py
@@ -7,4 +7,3 @@ def shorten(request, url):
     shortener = pyshorteners.Shortener()
     shortened_url = shortener.chilpit.short(url)
     return HttpResponse(f"Shortened URL: <a href='{shortened_url}'>{shortened_url}</a>")
-
