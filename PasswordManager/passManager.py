43fba4789183556b4235c98076bc5723842544e0
Derek <dduong03@uoguelph.ca>
2024-01-28 13:22:17 -0500

    able to add and view passwords in a file

--- PasswordManager/passManager.py
+++ PasswordManager/passManager.py
@@ -1,14 +1,18 @@
 master_pwd = input("What is the master password? ")
 
 def view():
-    pass
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            data = line.strip()
+            usr, pwrd = data.split("|")
+            print(f"User: {usr}\nPassword: {pwrd}")
 
 def add():
     name = input("Account Name: ")
     password = input("Password: ")
     
     #append mode
-    with open("passowrds.txt", "a") as f:
+    with open("passwords.txt", "a") as f:
         f.write(name + " | " + password + "\n")
         
 
