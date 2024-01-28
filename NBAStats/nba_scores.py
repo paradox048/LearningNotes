44c9ccce4b3f31cd671170a101497ff32c78052c
Derek <dduong03@uoguelph.ca>
2024-01-28 15:32:45 -0500

    Removed NBA project and added script data project

--- NBAStats/nba_scores.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-from pprint import PrettyPrinters
-
-BASE_URL = "https://data.nba.net"
-ALL_JSON = "/prod/v1/today.json"
-
-response = get(BASE_URL + ALL_JSON).json()
-
-print(response)
